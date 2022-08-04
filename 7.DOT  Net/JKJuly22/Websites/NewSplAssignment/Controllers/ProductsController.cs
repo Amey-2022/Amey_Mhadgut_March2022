0b863158de0d38356dfdaba0e6fde2104f1c108f
Amey-2022 <amameymhadgut132@gmail.com>
2022-08-05 00:32:14 +0530

    .net

---
+++ 7.DOT  Net/JKJuly22/Websites/NewSplAssignment/Controllers/ProductsController.cs
@@ -0,0 +1,105 @@
+﻿using SplAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewSplAssignment.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Product> lstProduct = Product.GetProducts();
+
+            return View(lstProduct);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            Product obj=Product.GetSingleProduct(id);
+            return View(obj);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var list = Category.GetCategoryNames();
+            ViewBag.list = list;
+            Product obj = Product.GetSingleProduct(id);
+            return View(obj);
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit( Product obj)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Product.UpdateProduct(obj);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        public ActionResult PartialDisplay()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ 7.DOT  Net/JKJuly22/Websites/NewSplAssignment/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NewSplAssignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("NewSplAssignment")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b313151-21b0-4717-8cb7-b5184b57b6f8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
